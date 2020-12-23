d526fa3431f09704e594a94a54382b632171f224
Adam Green <adam.green@kcl.ac.uk>
2020-12-23 23:21:06 +0000

    fixed setup, added dist to ignore and a few minor tweaks

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name='monkeys-typewriter',
       version=open('VERSION').read(),
       description="Monkey's Typewriter PEG parser",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Adam Green',
       author_email='adam@tryal.ai',
+      donwload_url='https://github.com/tryal-ai/mnkytw/archive/1.0.0.tar.gz',
       url='https://tryal.ai/',
       packages=find_packages(),
-      install_requires=[]
+      keywords=["PEG", "parser", "grammar"],
+      python_requires='>=3.6',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
